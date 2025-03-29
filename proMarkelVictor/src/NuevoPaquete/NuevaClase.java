83279c8fccaebeaba592590bac8a8186e96e1686
Usuario <Usuario@VictorPC>
2025-03-29 15:17:50 +0100

    Nuevo Paquete en La nueva Rama

---
+++ proMarkelVictor/src/NuevoPaquete/NuevaClase.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NuevoPaquete;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NuevaClase {
+     public static void main(String[] args){
+         
+     }
+}
